836770bf9c26efb02adbd0a3e24c7345e75aab3d
Evgeny Usorov <eusorov@gmail.com>
2018-02-07 23:44:57 +0100

    remove secrets

--- config/secrets.js
+++ config/secrets.js
@@ -8,46 +8,46 @@ module.exports = {
 
   cryptos: {
     algorithm: 'aes256',
-    key: process.env.CRYPTO_KEY || 'computer'
+    key: process.env.CRYPTO_KEY || ''
   },
 
   mail : {
-    gguser: 'eusorov@googlemail.com',
+    gguser: '',
     ggkey: ''
   },
 
   proxy : process.env.PROXY || '',
 
   recaptcha: {
-    secretKey : '6LdscQoUAAAAAKRwKc7GNp1z6S7oiA6Id9X-Jllq'
+    secretKey : ''
   },
 
 
-  sessionSecret: process.env.SESSION_SECRET || 'computer',
+  sessionSecret: process.env.SESSION_SECRET || '',
 
   twitter: {
     consumerKey: process.env.TWITTER_KEY || 'Your Twitter consumer key',
     consumerSecret: process.env.TWITTER_SECRET  || 'Your Twitter consumer secret',
-    callbackURL: process.env.TWITTER_CALLBACK || 'http://127.0.0.1:3000/auth/twitter/callback',
+    callbackURL: process.env.TWITTER_CALLBACK || '/auth/twitter/callback',
     passReqToCallback: true
   },
 
   facebook : {
-        clientID        : '254639964877043', // your App ID
-        clientSecret    : '16707fd504e63e25fc2e2469f9fcda33', // your App Secret
-        callbackURL     : host+'/auth/facebook/callback'
+        clientID        : '', // your App ID
+        clientSecret    : '', // your App Secret
+        callbackURL     : '/auth/facebook/callback'
     },
 
   google : {
-        clientID         : '350472840950-h05tsr332j77jc7qbdjgl7fi2v5qsbl9.apps.googleusercontent.com',
-        clientSecret     : 'ZXD6lEuBPnyFkXvS09PqQnKK',
-        callbackURL      : host+'/auth/google/callback'
+        clientID         : '',
+        clientSecret     : '',
+        callbackURL      : '/auth/google/callback'
     },
 
   github: {
-    clientID: 'f541b1860971793d0e6e',
-    clientSecret: 'f5c5122b20cf9f8a47e322560159ac2c25492c0a',
-    callbackURL: host+"/auth/github/callback"
+    clientID: '',
+    clientSecret: '',
+    callbackURL: "/auth/github/callback"
   },
   linkedin: {
     clientID: 'get_your_own',
